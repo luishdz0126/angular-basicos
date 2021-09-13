0fd7041d391b89aa619b0e9ee41714e3f5061047
Luis Hernandez <luis.hp.lp@gmail.com>
2021-09-12 20:57:23 -0500

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { ContadorComponente } from './contador/contador.component';
+
+@NgModule({
+    declarations: [
+        ContadorComponente
+    ],
+    exports: [
+        ContadorComponente
+    ],
+    imports: [
+      
+    ]
+
+})
+export class ContadorModule {}
